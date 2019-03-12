8b0877ff127535f800a52f75c3c133112c9156f0
Abhishek Jaiswal <jaiswala@usc.edu>
2019-03-12 14:28:47 -0700

    first commit with two tests

--- XUnitTestProject/UnitTest1.cs
+++ XUnitTestProject/UnitTest1.cs
@@ -8,7 +8,13 @@ namespace XUnitTestProject
         [Fact]
         public void Test1()
         {
+            Assert.True(1 == 1);
+        }
 
+        [Fact]
+        public void Test2()
+        {
+            Assert.True(2 == 1);
         }
     }
 }
